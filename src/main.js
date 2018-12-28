36bc2a4014b76680281a324c39aa56a6b65e9684
jdiaz-stratio <jdiaz@stratio.com>
2018-12-28 12:08:04 +0100

    init repo

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// import Vue from 'vue'
+// import App from './App.vue'
+
+// Vue.config.productionTip = false
+
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+
+import Vue from 'vue';
+import wrap from  '@vue/web-component-wrapper';
+import VueWebComponent from './App';
+
+const CustomElement = wrap(Vue, VueWebComponent);
+
+window.customElements.define('my-custom-element', CustomElement);
